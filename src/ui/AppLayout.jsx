f5efe80d2c4ac95373ac1ba6d705f83c49e9d376
Tamás Nagy <ntamasalbert@gmail.com>
2024-08-01 06:51:16 +0200

    add loading menu data, error handling

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,25 @@
+import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet, useNavigate } from "react-router-dom";
+import Loader from "./Loader";
+
+function AppLayout() {
+  const navigation = useNavigate();
+  const isLoading = navigation.state === "loading";
+
+  return (
+    <div className="layout">
+      {isLoading && <Loader />}
+
+      <Header />
+
+      <main>
+        <Outlet />
+      </main>
+
+      <CartOverview />
+    </div>
+  );
+}
+
+export default AppLayout;

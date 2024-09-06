86bf7489e88f67d78c51202dd23eaa92477da14e
Tamás Nagy <ntamasalbert@gmail.com>
2024-09-06 10:46:16 +0200

    add functionality to add item to cart

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -30,7 +30,8 @@ const fakeCart = [
 
 function Cart() {
   const username = useSelector((state) => state.user.username);
-  const cart = fakeCart;
+  // const cart = fakeCart;
+  const cart = useSelector((state) => state.cart.cart);
 
   return (
     <div className="px-4 py-3">
